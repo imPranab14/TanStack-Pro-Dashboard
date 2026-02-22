20fd06d381e36db645b0a8863446462bebec167b
Pranab Quila <pranab.quila@gmail.com>
2026-02-22 21:33:03 +0530

    dashboard api re fetch every 5 sec interval

--- src/features/dashboard/page/Dashboard.jsx
+++ src/features/dashboard/page/Dashboard.jsx
@@ -17,6 +17,7 @@ function Dashboard() {
   const { data: dashboardData, isLoading } = useQuery({
     queryKey: ["dashboard"],
     queryFn: fetchDashboard,
+    refetchInterval:5 * 1000 //Refetch in every 5 sec 
   });
   console.log("data", dashboardData);
   return (
@@ -27,8 +28,15 @@ function Dashboard() {
         Array.isArray(dashboardData) &&
         dashboardData?.map((ele, id) => {
           return (
-            <div key={id}>
+        <div key={id} style={{border:"1px solid red"}}>
               <p>{ele.Ticket_ID ?? "NA"}</p>
+              <p>{ele.Date_Created ?? "NA"}</p>
+              <p>{ele.Customer_Name ?? "NA"}</p>
+              <p>{ele.Agent_Name ?? "NA"}</p>
+              <p>{ele.CSAT_Score ?? "NA"}</p>
+              <p>{ele.Category ?? "NA"}</p>
+              <p>{ele.Priority ?? "NA"}</p>
+              <p>{ele.Status ?? "NA"}</p>
             </div>
           );
         })}
