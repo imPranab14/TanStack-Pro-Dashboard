3ae458f7a562d91acc15f1eaa75102a2524701d4
Pranab Quila <pranab.quila@gmail.com>
2026-03-04 23:41:35 +0530

    sw modifited

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,17 @@
 self.addEventListener("push", (event) => {
-  console.log("Push event received");
+  console.log("Push received");
+
+  const data = event.data ? event.data.json() : {};
+
+  const title = data.title || "Notification";
+  const options = {
+    body: data.body || "You have a new notification",
+    // icon: "/logo192.png",
+    // badge: "/logo192.png",
+    vibrate: [200, 100, 200],
+  };
 
   event.waitUntil(
-    self.registration.showNotification("Manual Push Test", {
-      body: "If you see this, SW works.",
-      //icon: "/logo192.png",
-      requireInteraction: true,
-      //vibrate: [300, 100, 300],
-      //silent: false,
-    }),
+    self.registration.showNotification(title, options)
   );
 });
