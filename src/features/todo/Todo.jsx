79f15f385c01155748352488f3e3016a6231093a
imPranab14 <pranab.quila@gmail.com>
2026-03-05 17:40:13 +0530

    notification tosted added

--- src/features/todo/Todo.jsx
+++ src/features/todo/Todo.jsx
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import { insertTodo } from "./api/todo.api";
+import toast, { Toaster } from "react-hot-toast";
 
 function Todo() {
   const [data, setData] = useState(null);
+
   async function handelSubmit(e) {
     e.preventDefault();
-   
-    //IMPORTANT Insert DB 
+    //IMPORTANT Insert DB
     try {
-      const response= await insertTodo(data)
-      console.log("api_res",response);
-      // if(response.status === 201){
-
-      // }
+      const response = await insertTodo(data);
+      console.log("api_res", response);
+      //Toast Notification
+      if (response.status === 201) {
+        toast.success("Task Added Successfully");
+      }
     } catch (error) {
-      console.log("failed to insert",error);
-      
+      console.log("failed to insert", error);
     }
   }
   return (
@@ -30,6 +31,7 @@ function Todo() {
         />
         <input type="submit" />
       </form>
+      <Toaster />
     </>
   );
 }
