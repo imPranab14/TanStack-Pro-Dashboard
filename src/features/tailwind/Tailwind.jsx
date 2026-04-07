efbde7a3a47473a2322d9769b5edf9b40859a02f
imPranab14 <pranab.quila@gmail.com>
2026-04-07 17:44:32 +0530

    flex section done

--- src/features/tailwind/Tailwind.jsx
+++ src/features/tailwind/Tailwind.jsx
@@ -11,9 +11,20 @@ function Tailwind() {
 
       <div className="w-screen bg-orange-300">Full Width Section</div>
 
+      {/* NOTE Flex  */}
+      {/* Flexbox (1D Layout)    (row OR column) */}
+      <div class="flex space-x-4 gap-2 bg-gray-400 flex-col">
+        <div className="bg-orange-600">1</div>
+        <div className="bg-pink-600">3</div>
+      </div>
 
-
-      
+      <div class="flex justify-between items-center p-4 bg-gray-200">
+        <h1>Logo</h1>
+        <div class="flex gap-4">
+          <a>Home</a>
+          <a>About</a>
+        </div>
+      </div>
     </>
   );
 }
