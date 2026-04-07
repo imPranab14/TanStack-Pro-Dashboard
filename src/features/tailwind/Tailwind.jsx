d90131969c44cf650ecb39e6140cb9172fb64414
imPranab14 <pranab.quila@gmail.com>
2026-04-07 17:29:17 +0530

    tailwind container section done

--- src/features/tailwind/Tailwind.jsx
+++ src/features/tailwind/Tailwind.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Tailwind() {
   return (
     <>
-    <h1>Tailwind</h1>
+      <h1>Tailwind</h1>
+
+      <div className="container bg-amber-300 mx-auto">Header Container</div>
+
+      <div className="w-1/2 bg-green-300">Width Section (50%)</div>
+
+      <div className="w-screen bg-orange-300">Full Width Section</div>
+
+
+
+      
     </>
-  )
+  );
 }
 
-export default Tailwind
+export default Tailwind;
